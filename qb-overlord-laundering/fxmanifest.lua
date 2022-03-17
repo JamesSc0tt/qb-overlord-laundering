043bf51c11ed1a9c89ed753437d8fa79b173a243
Magnus <64981416+MMagnusss@users.noreply.github.com>
2022-03-17 19:51:33 +0100

    Update fxmanifest.lua

--- qb-overlord-laundering/fxmanifest.lua
+++ qb-overlord-laundering/fxmanifest.lua
@@ -5,7 +5,13 @@ description 'QB-Overlord-Laundering'
 version '1.0.0'
 
 shared_scripts { 
-	'@qb-core/import.lua',
+	'@qb-core/shared/gangs.lua',
+	'@qb-core/shared/items.lua',
+	'@qb-core/shared/jobs.lua',
+	'@qb-core/shared/locale.lua',
+	'@qb-core/shared/main.lua',
+	'@qb-core/shared/vehicles.lua',
+	'@qb-core/shared/weapons.lua',
 	'config.lua'
 }
 
